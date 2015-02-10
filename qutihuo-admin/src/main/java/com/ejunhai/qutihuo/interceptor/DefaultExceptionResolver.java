ae28cc9518aaf30e447e73713a0de7d78e31e304
罗正加 <luozj@ucweb.com>
2015-02-10 15:32:22 +0800

    实现登陆功能

--- qutihuo-admin/src/main/java/com/ejunhai/qutihuo/interceptor/DefaultExceptionResolver.java
+++ qutihuo-admin/src/main/java/com/ejunhai/qutihuo/interceptor/DefaultExceptionResolver.java
@@ -1,5 +1,10 @@
 package com.ejunhai.qutihuo.interceptor;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,6 +14,9 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ejunhai.qutihuo.errors.BusinessException;
+import com.ejunhai.qutihuo.errors.ErrorType;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * 系统默认异常处理类
@@ -21,16 +29,34 @@ public class DefaultExceptionResolver implements HandlerExceptionResolver {
 	/** Logger available to subclasses */
 	protected final Logger logger = Logger.getLogger(this.getClass().getName());
 
+	protected final Gson gson = new GsonBuilder().serializeNulls().disableHtmlEscaping().create();
+
 	@Override
 	public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
 			Exception ex) {
 		// 打印错误日志
 		logger.error("接口异常", ex);
 
-		// 处理异步请求
-		if (StringUtils.isNotBlank(request.getHeader("X-Requested-With"))) {
-			// 重定向到统一异常处理
-			return new ModelAndView("redirect:/toList");
+		// 处理异步请求-重定向到统一异常处理
+		if (this.isAjax(request)) {
+
+			// 分析异常类型
+			Integer errorCode = ErrorType.SYSTEM_BUSY.getValue();
+			String errorMsg = ErrorType.SYSTEM_BUSY.getTitle();
+			if (ex instanceof BusinessException) {
+				BusinessException businessException = (BusinessException) ex;
+				errorCode = businessException.getErrorCode();
+				errorMsg = businessException.getMessage();
+			}
+
+			try {
+				response.setContentType("text/html; charset=utf-8");
+				PrintWriter writer = response.getWriter();
+				writer.write(this.renderJson(errorCode, errorMsg, null));
+				writer.flush();
+			} catch (IOException e) {
+				logger.error("处理异常请求出错", e);
+			}
 		}
 
 		// 根据不同错误转向不同页面
@@ -39,5 +65,21 @@ public class DefaultExceptionResolver implements HandlerExceptionResolver {
 		}
 
 		return new ModelAndView("error-404");
+	}
+
+	private boolean isAjax(HttpServletRequest request) {
+		String xRequestedWith = request.getHeader("X-Requested-With");
+		boolean isAjax = request.getHeader("accept").indexOf("application/json") > -1;
+		return isAjax || (xRequestedWith != null && xRequestedWith.indexOf("XMLHttpRequest") > -1);
+	}
+
+	private <T> String renderJson(int code, String message, T data) {
+		Map<String, Object> jsonMap = new HashMap<String, Object>();
+		Map<String, Object> stateMap = new HashMap<String, Object>();
+		stateMap.put("code", code);
+		stateMap.put("msg", StringUtils.isBlank(message) ? "Ok" : message);
+		jsonMap.put("state", stateMap);
+		jsonMap.put("data", data);
+		return gson.toJson(jsonMap);
 	}
 }
