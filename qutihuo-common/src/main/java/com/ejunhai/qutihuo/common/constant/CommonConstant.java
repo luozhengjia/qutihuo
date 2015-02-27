ae7e06ad308ab9744d721382e0fbec1a7e079aab
sheyj <she840707@163.com>
2015-02-27 16:14:59 +0800

    订单号规则开发

--- qutihuo-common/src/main/java/com/ejunhai/qutihuo/common/constant/CommonConstant.java
+++ qutihuo-common/src/main/java/com/ejunhai/qutihuo/common/constant/CommonConstant.java
@@ -5,5 +5,9 @@ public class CommonConstant {
 	 * 顶级菜单ID
 	 */
 	public static final Integer ROOT_MENU_ID = 0;
-
+	
+	/**
+	 * 订单号前缀
+	 */
+	public static final String ORDER_NO_PREFIX = "D";
 }
--- qutihuo-common/src/main/java/com/ejunhai/qutihuo/order/service/impl/OrderMainServiceImpl.java
+++ qutihuo-common/src/main/java/com/ejunhai/qutihuo/order/service/impl/OrderMainServiceImpl.java
@@ -6,6 +6,8 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.ejunhai.qutihuo.common.constant.CommonConstant;
+import com.ejunhai.qutihuo.order.dao.CodingRuleMapper;
 import com.ejunhai.qutihuo.order.dao.OrderMainMapper;
 import com.ejunhai.qutihuo.order.model.OrderMain;
 import com.ejunhai.qutihuo.order.service.OrderMainService;
@@ -21,39 +23,44 @@ import com.ejunhai.qutihuo.order.service.OrderMainService;
 @Service("orderMainService")
 public class OrderMainServiceImpl implements OrderMainService {
 
-    @Resource
-    private OrderMainMapper orderMainMapper;
+	@Resource
+	private OrderMainMapper orderMainMapper;
 
-    @Override
-    public OrderMain read(Integer id) {
-        return orderMainMapper.read(id);
-    }
+	@Resource
+	private CodingRuleMapper codingRuleMapper;
 
-    @Override
-    public void insert(OrderMain orderMain) {
-    	orderMainMapper.insert(orderMain);
-    }
+	@Override
+	public OrderMain read(Integer id) {
+		return orderMainMapper.read(id);
+	}
 
-    @Override
-    public void update(OrderMain orderMain) {
-    	orderMainMapper.update(orderMain);
+	@Override
+	public void insert(OrderMain orderMain) {
+		//设置订单号
+		orderMain.setOrderMainNo(CommonConstant.ORDER_NO_PREFIX + codingRuleMapper.getOrderNo());
+		orderMainMapper.insert(orderMain);
+	}
 
-    }
+	@Override
+	public void update(OrderMain orderMain) {
+		orderMainMapper.update(orderMain);
 
-    @Override
-    public void delete(Integer id) {
-    	orderMainMapper.delete(id);
+	}
 
-    }
+	@Override
+	public void delete(Integer id) {
+		orderMainMapper.delete(id);
 
-    @Override
-    public Integer queryOrderMainCount(OrderMain orderMain) {
-        return orderMainMapper.queryOrderMainCount(orderMain);
-    }
+	}
 
-    @Override
-    public List<OrderMain> queryOrderMainList(OrderMain orderMain) {
-        return orderMainMapper.queryOrderMainList(orderMain);
-    }
+	@Override
+	public Integer queryOrderMainCount(OrderMain orderMain) {
+		return orderMainMapper.queryOrderMainCount(orderMain);
+	}
+
+	@Override
+	public List<OrderMain> queryOrderMainList(OrderMain orderMain) {
+		return orderMainMapper.queryOrderMainList(orderMain);
+	}
 
 }
