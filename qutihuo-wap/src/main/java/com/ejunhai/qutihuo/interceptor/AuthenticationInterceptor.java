48b531e4a6b9444546a47637c0691fbd9cdbd96e
罗正加 <luozhengjia@shangquanquan.com>
2018-08-28 21:09:20 +0800

    修复bug

--- qutihuo-wap/src/main/java/com/ejunhai/qutihuo/interceptor/AuthenticationInterceptor.java
+++ qutihuo-wap/src/main/java/com/ejunhai/qutihuo/interceptor/AuthenticationInterceptor.java
@@ -3,37 +3,47 @@ package com.ejunhai.qutihuo.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ejunhai.qutihuo.merchant.service.MerchantService;
 import com.ejunhai.qutihuo.utils.FrontUtil;
 import com.ejunhai.qutihuo.utils.SessionManager;
 
 public class AuthenticationInterceptor implements HandlerInterceptor {
 
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
+    @Autowired
+    private MerchantService merchantService;
 
-		if (FrontUtil.isExcludeUrl(request.getRequestURI())) {
-			return true;
-		}
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
 
-		// 若用户未登录，则跳转至登陆页
-		if (SessionManager.get(request) == null) {
-			response.sendRedirect("/index.jhtml");
-			return false;
-		}
-		return true;
-	}
+        if (FrontUtil.isExcludeUrl(request.getRequestURI())) {
+            return true;
+        }
 
-	@Override
-	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
-			throws Exception {
-	}
+        // 若用户未登录，则跳转至登陆页
+        if (SessionManager.get(request) == null) {
+            response.sendRedirect("/index.jhtml");
+            return false;
+        }
 
-	@Override
-	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
-			throws Exception {
-	}
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3) throws Exception {
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse arg1, Object arg2, ModelAndView arg3) throws Exception {
+        // 处理异步请求-无需准备菜单数据
+        if (FrontUtil.isAjax(request) || SessionManager.get(request) == null) {
+            return;
+        }
+
+        arg3.addObject("_user", merchantService.read(SessionManager.get(request).getMerchantId()));
+    }
 
 }
--- qutihuo-wap/src/main/webapp/common/footer.ftl
+++ qutihuo-wap/src/main/webapp/common/footer.ftl
@@ -1,3 +1,3 @@
 <div class="navbar navbar-default">
-	<p class="navbar-text text-center" style="width:100%"><small> 武汉壹骏海实业有限公司 | 鄂ICP备12011799号-1<br> 如遇困难可拨打 07115113338 </small></p>
+	<p class="navbar-text text-center" style="width:100%"><small>${_user.merchantName!''} | 鄂ICP备12011799号-1<br> 如遇困难可拨打 ${_user.recordNumber!''} </small></p>
 </div>
